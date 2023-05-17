03a357d8e52328a922d9f30f1808fc9bde5be1b6
logkyung <aebong54@gmail.com>
2023-05-17 23:26:50 +0900

    Update follow and search function

--- podomarket/urls.py
+++ podomarket/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     # posts
     path('', views.IndexView.as_view(), name='index'),
+    path('wishlist/', views.WishlistView.as_view(), name='wishlist'),
+    path('posts/following/', views.FollowingPostListView.as_view(), name='following-post-list'),
+    path('search/', views.SearchView.as_view(), name='search'),
     path('posts/<int:post_id>/', views.PostDetailView.as_view(), name="post-detail"),
     path('posts/new/', views.PostCreateView.as_view(), name="post-create"),
     path('posts/<int:post_id>/edit/', views.PostUpdateView.as_view(), name="post-update"),
@@ -17,4 +20,12 @@ urlpatterns = [
 
     #comment
     path('posts/<int:post_id>/comments/create/', views.CommentCreateView.as_view(), name="comment-create"),
+    path('comments/<int:comment_id>/edit/', views.CommentUpdateView.as_view(), name='comment-update'),
+    path('comments/<int:comment_id>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
+
+    #like
+    path('like/<int:content_type_id>/<int:object_id>/', views.ProcessLikeView.as_view(), name='process-like'),
+
+    #follow
+    path('users/<int:user_id>/follow/', views.ProcessFollowView.as_view(), name='process-follow'),
 ]
--- podomarket/views.py
+++ podomarket/views.py
@@ -1,19 +1,22 @@
 from typing import Any, Dict
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.views.generic import (
+    View,
     ListView, 
     DetailView, 
     CreateView,
     UpdateView,
     DeleteView,
 )
-from braces.views import LoginRequiredMixin, UserPassesTestMixin
-from allauth.account.forms import EmailAddress
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponse
+from django.db.models import Q
+from braces.views import LoginRequiredMixin
 from allauth.account.views import PasswordChangeView
-from .models import Post, User, Comment
+from .mixins import LoginAndVerificationRequiredMixin, LoginAndOwnershipRequiredMixin
+from .models import Post, User, Comment, Like
 from .forms import PostCreateForm, PostUpdateForm, ProfileForm, CommentForm
-from .functions import confirmation_required_redirect
 
 # Create your views here.
 class IndexView(ListView):
@@ -26,6 +29,50 @@ class IndexView(ListView):
         return Post.objects.filter(is_sold=False)
 
 
+class WishlistView(LoginRequiredMixin, ListView):
+    model = Post
+    context_object_name = 'liked_posts'
+    template_name = 'podomarket/wishlist.html'
+    paginate_by = 8
+
+    def get_queryset(self):
+        return Post.objects.filter(likes__user=self.request.user)
+
+
+class FollowingPostListView(LoginRequiredMixin, ListView):
+    model = Post
+    context_object_name = 'following_posts'
+    template_name = 'podomarket/following_post_list.html'
+    paginate_by = 8
+
+    def get_queryset(self):
+        return Post.objects.filter(is_sold=False, author__followers=self.request.user)
+
+
+def search_view(request):
+    query = request.GET.get('query', '')
+    return HttpResponse(f"검색어: {query}")
+
+
+class SearchView(ListView):
+    model = Post
+    context_object_name = 'search_results'
+    template_name = 'podomarket/search_results.html'
+    paginate_by = 8
+
+    def get_queryset(self):
+        query = self.request.GET.get('query', '')
+        return Post.objects.filter(
+            Q(title__icontains=query)
+            | Q(item_details__icontains=query)
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.request.GET.get('query', '')
+        return context
+
+
 class PostDetailView(DetailView):
     model = Post
     template_name = 'podomarket/post_detail.html'
@@ -34,17 +81,22 @@ class PostDetailView(DetailView):
     def get_context_data(self, **kwargs: Any):
         context = super().get_context_data(**kwargs)
         context['form'] = CommentForm()
+        context['post_ctype_id'] = ContentType.objects.get(model='post').id
+        context['comment_ctype_id'] = ContentType.objects.get(model='comment').id
+
+        user = self.request.user
+        if user.is_authenticated:
+            post = self.object
+            context['likes_post'] = Like.objects.filter(user=user, post=post)
+            context['liked_comments'] = Comment.objects.filter(post=post).filter(likes__user=user)
         return context
 
 
-class PostCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+class PostCreateView(LoginAndVerificationRequiredMixin, CreateView):
     model = Post
     form_class = PostCreateForm
     template_name = 'podomarket/post_form.html'
 
-    redirect_unauthenticated_users = True
-    raise_exception = confirmation_required_redirect
-    
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
@@ -52,49 +104,31 @@ class PostCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     def get_success_url(self):
         return reverse('post-detail', kwargs={"post_id":self.object.id})
 
-    def test_func(self, user):
-        return EmailAddress.objects.filter(user=user, verified=True).exists()
 
-
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+class PostUpdateView(LoginAndOwnershipRequiredMixin, UpdateView):
     model = Post
     form_class = PostUpdateForm
     template_name = 'podomarket/post_form.html'
     pk_url_kwarg = 'post_id'
 
-    raise_exception = True
-
     def get_success_url(self):
         return reverse('post-detail', kwargs={"post_id":self.object.id})
 
-    def test_func(self, user):
-        post = self.get_object()
-        return post.author == user
 
-
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+class PostDeleteView(LoginAndOwnershipRequiredMixin, DeleteView):
     model = Post
     template_name = 'podomarket/post_confirm_delete.html'
     pk_url_kwarg = 'post_id'
 
-    raise_exception = True
-
     def get_success_url(self):
         return reverse('index')
 
-    def test_func(self, user):
-        post = self.get_object()
-        return post.author == user
 
-
-class CommentCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+class CommentCreateView(LoginAndVerificationRequiredMixin, CreateView):
     http_method_names = ['post']
 
     model = Comment
     form_class = CommentForm
-
-    redirect_unauthenticated_users = True
-    raise_exception = confirmation_required_redirect
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -104,8 +138,40 @@ class CommentCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     def get_success_url(self):
         return reverse('post-detail', kwargs={'post_id':self.kwargs.get('post_id')})
 
-    def test_func(self, user):
-        return EmailAddress.objects.filter(user=user, verified=True).exists()
+
+class CommentUpdateView(LoginAndOwnershipRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'podomarket/comment_update_form.html'
+    pk_url_kwarg = 'comment_id'
+
+    def get_success_url(self):
+        return reverse('post-detail', kwargs={'post_id':self.object.post.id})
+
+
+class CommentDeleteView(LoginAndOwnershipRequiredMixin, DeleteView):
+    model = Comment
+    template_name = 'podomarket/comment_confirm_delete.html'
+    pk_url_kwarg = 'comment_id'
+    
+    def get_success_url(self):
+        return reverse('post-detail', kwargs={'post_id':self.object.post.id})
+
+
+class ProcessLikeView(LoginAndVerificationRequiredMixin, View):
+    http_method_names = ['post']
+
+    def post(self, request, *args, **kwargs):
+        like, created = Like.objects.get_or_create(
+            user = self.request.user,
+            content_type_id = self.kwargs.get('content_type_id'),
+            object_id = self.kwargs.get('object_id')
+        )
+
+        if not created:
+            like.delete()
+
+        return redirect(self.request.META['HTTP_REFERER'])
 
 
 class ProfileView(DetailView):
@@ -116,9 +182,25 @@ class ProfileView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        user_id = self.kwargs.get('user_id')
-        context["user_posts"] = Post.objects.filter(author__id=user_id)[:8]
+        user = self.request.user
+        profile_user_id = self.kwargs.get('user_id')
+        if user.is_authenticated:
+            context["is_following"] = user.following.filter(id=profile_user_id).exists()
+        context["user_posts"] = Post.objects.filter(author__id=profile_user_id)[:8]
         return context
+
+
+class ProcessFollowView(LoginAndVerificationRequiredMixin, View):
+    http_method_names = ['post']
+
+    def post(self, request, *args, **kwargs):
+        user = self.request.user
+        profile_user_id = self.kwargs.get('user_id')
+        if user.following.filter(id=profile_user_id).exists():
+            user.following.remove(profile_user_id)
+        else:
+            user.following.add(profile_user_id)
+        return redirect('profile', user_id=profile_user_id)
 
 
 class UserPostListView(ListView):
